4c9f39cf647c4c0b4d4b32044ea3e5a2d6a4c745
Sugam <ktw72@txstate.edu>
2026-02-04 15:46:46 -0600

    Improve hangman loop and fix repeated guess logic

--- main.py
+++ main.py
@@ -4,18 +4,30 @@ word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 print(chosen_word)
 
-# TODO-1: Create a "placeholder" with the same number of blanks as the chosen_word
-placeholder=""
-for letter in range(0,len(chosen_word)):
-    placeholder+="-"
+placeholder = ""
+word_length = len(chosen_word)
+for position in range(word_length):
+    placeholder += "_"
 print(placeholder)
-guess = input("Guess a letter: ").lower()
 
-# TODO-2: Create a "display" that puts the guess letter in the right positions and _ in the rest of the string.
-display=""
-for letter in chosen_word:
-    if letter==guess:
-        display+=letter
-    else:
-        display+="_"
-print(display)
+# TODO-1: - Use a while loop to let the user guess again.
+game_over= False
+correct_letters=[]
+while not game_over :
+    guess = input("Guess a letter: ").lower()
+    display = ""
+# TODO-2: Change the for loop so that you keep the previous correct letters in display.
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            correct_letters.append(letter)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
+
+    print(display)
+
+    if "_" not in display:
+        game_over= True
+        print("You win!")
